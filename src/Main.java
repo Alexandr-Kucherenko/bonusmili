b642990aed010418c739bc300fede4c4db3eb0e0
Kucherenko Alexandr <alexandr.kucherenko1977@gmail.com>
2023-04-21 19:21:10 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 20000;
+        int bonus = 20;
+        int mil = num / bonus;
+
+        System.out.println(mil);
+    }
+}
